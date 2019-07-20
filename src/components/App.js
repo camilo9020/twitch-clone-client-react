9af0d6f673b0675468e22e95f064eb7c85f8bc41
Juan Camilo Hernández <camilo9020@gmail.com>
2019-07-19 21:17:48 -0500

    Create all app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
+
 
 class App extends React.Component {
   render() {
-    return <h1>App component</h1>
+    return (
+      <div className="ui container">
+        <BrowserRouter>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/show" component={StreamShow} />
+          <Route path="/streams/create" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
