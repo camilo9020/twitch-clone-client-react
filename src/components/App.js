74afe9d36c578fcf72199a017752f350a65f39fc
Juan Camilo Hernández <camilo9020@gmail.com>
2019-10-04 09:27:34 -0500

    Add Switch component on routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamList';
@@ -7,6 +7,7 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import Header from './Header';
 import history from '../history';
+import { Stream } from 'stream';
 
 
 class App extends React.Component {
@@ -15,11 +16,13 @@ class App extends React.Component {
       <div className="ui container">
         <Router history={history}>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/show" component={StreamShow} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
+            <Switch>
+              <Route path="/" exact component={StreamList} />
+              <Route path="/streams/new" exact component={StreamCreate} />
+              <Route path="/streams/edit/:id" exact component={StreamEdit} />
+              <Route path="/streams/delete/:id" exact component={StreamDelete} />
+              <Route path="/streams/:id" exact component={StreamShow} />
+            </Switch>
         </Router>
       </div>
     );
